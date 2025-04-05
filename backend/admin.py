6574a8b2ac3de100f6d1832001dd02188169a571
SubhasreeCodes <subhasreeps2001@gmail.com>
2025-04-05 11:09:48 +0530

    Update admin.py

--- backend/admin.py
+++ backend/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from django.utils.html import format_html
+
+from backend.models import Gallery
 
 # Register your models here.
+
+class GalleryAdmin(admin.ModelAdmin):
+    list_display = ('id', 'image_tag')
+
+    def image_tag(self, obj):
+        return format_html('<img src ="{}" width="150" height="150" />'.format(obj.image.url))
+
+    image_tag.short_description = "Image"
+
+admin.site.register(Gallery, GalleryAdmin)
